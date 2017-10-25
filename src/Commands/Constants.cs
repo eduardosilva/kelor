ae1f55c0972ef67284f9b43fbe746c9929e8435b
Eduardo Silva <eduardo.lour.silva@gmail.com>
2017-10-25 17:51:41 -0200

    started the project

---
+++ src/Commands/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kelor.Commands
+{
+    public static class Constants
+    {
+        public const string SOURCE = "-s";
+        public const string TO = "-d";
+        public const string NAME = "-n";
+        public const string ONLY_CONTENT = "-oc";
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kelor.Commands;
+
+namespace Kelor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = typeof(IKelorCommand).Assembly.GetTypes()
+                                                         .Where(t => t.IsAbstract == false &&
+                                                                     t.BaseType != null &&
+                                                                     t.BaseType == typeof(KelorCommand))
+                                                         .Select(t => (IKelorCommand)Activator.CreateInstance(t))
+                                                         .ToArray();
+
+            if (!args.Any())
+            {
+                Console.WriteLine("List of commands");
+                Console.WriteLine("");
+                foreach (var command in commands)
+                {
+                    Console.WriteLine("{0}: {1}", command.Code, command.Description);
+                }
+                return;
+            }
+
+            var localizedCommand = commands.FirstOrDefault(c => args.Contains(c.Code));
+            if (localizedCommand == null)
+            {
+                Console.WriteLine("kelor: '{0}' is not a kelor command. See kelex -help", args.FirstOrDefault());
+                return;
+            }
+
+            Console.WriteLine("");
+            localizedCommand.Execute(args);
+        }
+    }
+}
